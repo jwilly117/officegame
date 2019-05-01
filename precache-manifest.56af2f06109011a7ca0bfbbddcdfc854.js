d05b4675854265d0e28324ce11d8e00ab09da22c
Jake Williams <jakewilliams117@gmail.com>
2019-05-01 04:26:29 -0400

    Updates

---
+++ precache-manifest.56af2f06109011a7ca0bfbbddcdfc854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8905fbe756939f46932ec725cadb02",
+    "url": "/officegame/index.html"
+  },
+  {
+    "revision": "cd2aa6c18829aeaaa659",
+    "url": "/officegame/static/css/main.3b9fe88f.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/officegame/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "cd2aa6c18829aeaaa659",
+    "url": "/officegame/static/js/main.b8722ee0.chunk.js"
+  },
+  {
+    "revision": "64f72df525a22f0d6989",
+    "url": "/officegame/static/js/runtime~main.82c3dcde.js"
+  }
+]);
